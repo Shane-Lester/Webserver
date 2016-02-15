b6e6add5ca878eb4bb1e395d149c97f272001c98
Shane-Lester <shane_lester@hotmail.com>
2016-02-15 09:22:23 +0000

    First commit- initialize repo for Udemy node course webserver module

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+// app.get('/',function(req, res){
+// 	res.send('Hello Express');
+// });
+
+var middleware = {
+	requireAuthentication : function (req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next){
+		var date = new Date().toString();
+		console.log(date +" Request: " + req.method + " " + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication,function(req, res){
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log("Express server starting on port "+ PORT + "!");
+
+});
