83ca9b94bf6fabe4242421ecaff7ee97563c957a
Shane-Lester <shane_lester@hotmail.com>
2016-02-15 09:29:37 +0000

    using module exports to refactor

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var middleware = require('./middleware');
 // app.get('/',function(req, res){
 // 	res.send('Hello Express');
 // });
-
-var middleware = {
-	requireAuthentication : function (req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next){
-		var date = new Date().toString();
-		console.log(date +" Request: " + req.method + " " + req.originalUrl);
-		next();
-	}
-};
 
 app.use(middleware.logger);
 
